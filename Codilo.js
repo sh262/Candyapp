d2f24107af4a11d674ecfe32f7955d21ae70069e
Candystore <82749215+sh262@users.noreply.github.com>
2023-12-18 01:11:38 +0330

    Add files via upload

---
+++ Codilo.js
@@ -0,0 +1,75 @@
+// By candyteam (:
+
+console.clear();
+
+console.log(
+"                                                                                   \n" +
+"%c                                 @@       %c      @@                               \n" +
+"%c                            @@@@@@@@@@    %c   @@@@@@@@@@                          \n" +
+"%c                        @@@@@@@@@@@@@@    %c   @@@@@@@@@@@@@@/                     \n" +
+"%c                   /@@@@@@@@@@@@@@@@@     %c    @@@@@@@@@@@@@@@@@@                 \n" +
+"%c               @@@@@@@@@@@@@@@@@@         %c        @@@@@@@@@@@@@@@@@@             \n" +
+"%c           @@@@@@@@@@@@@@@@@@             %c            %@@@@@@@@@@@@@@@@@.        \n" +
+"%c         @@@@@@@@@@@@@@@%                 %c                 @@@@@@@@@@@@@@@       \n" +
+"%c         @@@@@@@@@@@@@@@(                 %c                 @@@@@@@@@@@@@@@       \n" +
+"%c           @@@@@@@@@@@@@@@@@@             %c            #@@@@@@@@@@@@@@@@@*        \n" +
+"%c               @@@@@@@@@@@@@@@@@@         %c        @@@@@@@@@@@@@@@@@@             \n" +
+"%c    &@@(           *@@@@@@@@@@@@@@@@@/    %c    @@@@@@@@@@@@@@@@@@           &@@&  \n" +
+"%c  @@@@@@@@@@            @@@@@@@@@@@@@@@@@@%c@@@@@@@@@@@@@@@@@/           @@@@@@@@@@\n" +
+"%c @@@@@@@@@@@@@@@@           @@@@@@@@@@@@@@%c@@@@@@@@@@@@@           ,@@@@@@@@@@@@@@\n" +
+"%c @@@@@@@@@@@@@@@@@@@@           &@@@@@@@@@%c@@@@@@@@@           @@@@@@@@@@@@@@@@@@@\n" +
+"%c @@@@@@@@@@@@@@@@@@@@@@@@            @@@@@%c@@@@%           @@@@@@@@@@@@@@@@@@@@@@@\n" +
+"%c @@@@@@@@@ ,@@@@@@@@@@@@@@@@@@            %c           ,@@@@@@@@@@@@@@@@@& @@@@@@@@\n" +
+"%c @@@@@@@@@      @@@@@@@@@@@@@@@@@@        %c       @@@@@@@@@@@@@@@@@@      @@@@@@@@\n" +
+"%c @@@@@@@@@          @@@@@@@@@@@@@@@@@@    %c   @@@@@@@@@@@@@@@@@@          @@@@@@@@\n" +
+"%c @@@@@@@@@              .@@@@@@@@@@@@@@   %c  @@@@@@@@@@@@@@%              @@@@@@@@\n" +
+"%c @@@@@@@@@                   @@@@@@@@@@   %c  @@@@@@@@@@,                  @@@@@@@@\n" +
+"%c @@@@@@@@@                    %@@@@@@@@   %c  @@@@@@@@@                    @@@@@@@@\n" +
+"%c @@@@@@@@@                    %@@@@@@@@   %c  @@@@@@@@@                    @@@@@@@@\n" +
+"%c  @@@@@@@                     %@@@@@@@@   %c  @@@@@@@@@                     @@@@@@(\n" +
+"%c                              %@@@@@@@@   %c  @@@@@@@@@                            \n" +
+"%c                              %@@@@@@@@   %c  @@@@@@@@@                            \n" +
+"%c            @@@@@             %@@@@@@@@   %c  @@@@@@@@@              @@@@          \n" +
+"%c          @@@@@@@@@@@         %@@@@@@@@   %c  @@@@@@@@@         @@@@@@@@@@@        \n" +
+"%c          @@@@@@@@@@@@@@@*    %@@@@@@@@   %c  @@@@@@@@@     @@@@@@@@@@@@@@@        \n" +
+"%c            @@@@@@@@@@@@@@@@@@%@@@@@@@@   %c  @@@@@@@@@ @@@@@@@@@@@@@@@@@&         \n" +
+"%c                @@@@@@@@@@@@@@@@@@@@@@@   %c  @@@@@@@@@@@@@@@@@@@@@@@,             \n" +
+"%c                    #@@@@@@@@@@@@@@@@@@   %c  @@@@@@@@@@@@@@@@@@@                  \n" +
+"%c                        .@@@@@@@@@@@@@@   %c  @@@@@@@@@@@@@@@                      \n" +
+"%c                             @@@@@@@@@@   %c  @@@@@@@@@@,                          \n" +
+"%c                                                                                   \n" +
+"%c                               https://Codilo.ir (:                                \n",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #D32F2F;font-weight: bold;");
